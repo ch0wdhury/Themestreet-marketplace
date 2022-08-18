b41d22b1b08e2afa0ed866f2fb52b1717e21e071
Ashiq Ali Chowdhury <43188066+ch0wdhury@users.noreply.github.com>
2022-08-18 18:16:23 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//scroll top show
+
+var btn = $('#back-top-button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
